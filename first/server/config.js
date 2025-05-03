b11c2551437360256b0e0780f4b2ecc7e637511e
Aman_Kumar2002 <amankumarengineer2002@gmail.com>
2025-05-03 19:16:50 +0530

    added config

---
+++ first/server/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    WINDOW_SIZE: 10,
+    TEST_API_URLS: {
+      p: "http://20.244.56.144/evaluation-service/primes",
+      f: "http://20.244.56.144/evaluation-service/fibo",
+      e: "http://20.244.56.144/evaluation-service/even",
+      r: "http://20.244.56.144/evaluation-service/rand",
+    },
+  };
+  
